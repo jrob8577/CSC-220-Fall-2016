1f5a2ef6e4192b262d60d52920247122b248c4d8
John Roberts <roberts.john@gmail.com>
2016-08-25 21:56:49 -0700

    HelloWorldApp

---
+++ lecture-one/HelloWorldApp.java
@@ -0,0 +1,15 @@
+/**
+ * This is a "doc comment", and starts with two *
+ *
+ * The HelloWorldApp class implements an application that displays
+ * the String "Hello World!" to the standard output
+ */
+
+public class HelloWorldApp {
+  /* This is a multiline
+     comment */
+  public static void main( String[] args ) {
+    // This is a single line comment that extends only to the end of the current line
+    System.out.println( "Hello World!" );
+  }
+}
