3fb3c2988e37221f4c162c7758c702ef4574d5cb
John Roberts <roberts.john@gmail.com>
2016-10-29 08:00:31 -0700

    deque interface example

---
+++ lecture-ten/DequeInterfaceExample.java
@@ -0,0 +1,41 @@
+
+public class DequeInterfaceExample {
+  public static void main( String[] args ) {
+    DequeInterface<String> myDeque = new LinkedDeque<String>();
+
+    myDeque.addToFront( "Jim" );
+    System.out.println( "addToFront Jim" );
+
+    myDeque.addToBack( "Jess" );
+    System.out.println( "addToBack Jess" );
+
+    myDeque.addToFront( "Jill" );
+    System.out.println( "addToFront Jill" );
+
+    myDeque.addToBack( "Jane" );
+    System.out.println( "addToBack Jane" );
+
+    String name = myDeque.getFront();
+    System.out.println( "Front is " + name );
+
+    myDeque.addToBack( name );
+    System.out.println( "addToBack " + name );
+
+    myDeque.removeFront();
+    System.out.println( "removeFront" );
+
+    String back = myDeque.removeBack();
+    System.out.println( "removeBack returned " + back );
+
+    myDeque.addToFront( back );
+    System.out.println( "addToFront " + back );
+
+    System.out.println( "Contents of Deque:" );
+
+    System.out.print( "null <-> " );
+    while( ! myDeque.isEmpty() ) {
+      System.out.print( myDeque.removeFront() + " <-> " );
+    }
+    System.out.println( "null" );
+  }
+}
---
+++ lecture-ten/LinkedDeque.java
@@ -0,0 +1,130 @@
+
+public class LinkedDeque< T > implements DequeInterface< T > {
+
+  private DequeNode firstNode;
+  private DequeNode lastNode;
+
+  public LinkedDeque() {
+    this.firstNode = null;
+    this.lastNode = null;
+  }
+
+  public void addToFront( T newEntry ) {
+    DequeNode node = new DequeNode( null, newEntry, this.firstNode );
+
+    if( isEmpty() ) {
+      this.lastNode = node;
+    } else {
+      this.firstNode.setPrevious( node );
+    }
+
+    this.firstNode = node;
+  }
+
+  public void addToBack( T newEntry ) {
+    DequeNode node = new DequeNode( this.lastNode, newEntry, null );
+
+    if( isEmpty() ) {
+      this.firstNode = node;
+    } else {
+      this.lastNode.setNext( node );
+    }
+
+    this.lastNode = node;
+  }
+
+  public T removeFront() {
+    T front = null;
+
+    if( ! isEmpty() ) {
+      front = this.firstNode.getData();
+      this.firstNode = this.firstNode.getNext();
+
+      if( this.firstNode == null ) {
+        this.lastNode = null;
+      } else {
+        this.firstNode.setPrevious( null );
+      }
+    }
+
+    return front;
+  }
+
+  public T removeBack() {
+    T back = null;
+
+    if( ! isEmpty() ) {
+      back = this.lastNode.getData();
+      this.lastNode = this.lastNode.getPrevious();
+
+      if( this.lastNode == null ) {
+        this.firstNode = null;
+      } else {
+        this.lastNode.setNext( null );
+      }
+    }
+
+    return back;
+  }
+
+  public T getFront() {
+    T front = null;
+
+    if( ! isEmpty() ) {
+      front = this.firstNode.getData();
+    }
+
+    return front;
+  }
+
+  public T getBack() {
+    T back = null;
+
+    if( ! isEmpty() ) {
+      back = this.lastNode.getData();
+    }
+
+    return back;
+  }
+
+  public boolean isEmpty() {
+    return this.firstNode == null;
+  }
+
+  public void clear() {
+    this.firstNode = null;
+    this.lastNode = null;
+  }
+
+  private class DequeNode {
+    private T data;
+    private DequeNode previous;
+    private DequeNode next;
+
+    public DequeNode( DequeNode previous, T data, DequeNode next ) {
+      this.data = data;
+      this.previous = previous;
+      this.next = next;
+    }
+
+    public void setPrevious( DequeNode node ) {
+      this.previous = node;
+    }
+
+    public DequeNode getPrevious() {
+      return this.previous;
+    }
+
+    public void setNext( DequeNode node ) {
+      this.next = node;
+    }
+
+    public DequeNode getNext() {
+      return this.next;
+    }
+
+    public T getData() {
+      return this.data;
+    }
+  }
+}
